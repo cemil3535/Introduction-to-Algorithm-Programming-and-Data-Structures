cee6a55c4bd6a9f4a041b23aa8f914f7cdcddad9
Cemil Ozcan <71188464+cemil3535@users.noreply.github.com>
2024-11-24 15:32:15 +0300

    Add files via upload

---
+++ BTKAkademiSwitchCase/BTKAkademiSwitchCase/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Security.Principal;
+
+int A = 10;
+int B = 20;
+
+Operations select = (Operations)(new Random().Next(1,4));
+
+switch (select)
+{
+    case Operations.Sum:
+        Console.WriteLine($"{A} + {B} = {A + B}");
+        break;
+    case Operations.Minus:
+        Console.WriteLine($"{A} - {B} = {A - B}");
+        break;
+    case Operations.Multiply:
+        Console.WriteLine($"{A} * {B} = {A * B}");
+        break;
+    case Operations.Divide:
+        Console.WriteLine($"{A} / {B} = {A / B}");
+        break;
+    default:
+        Console.WriteLine("\aGecersiz Islem!");
+        break;
+}
+
+
+enum Operations
+{
+    Sum = 1,
+    Minus = 2,
+    Multiply = 3,
+    Divide = 4
+}
+
+
+
+
+
+
+
