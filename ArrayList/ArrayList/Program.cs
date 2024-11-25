7e43d34d644c9836bbf6edad4716fc386732f3be
Cemil Ozcan <71188464+cemil3535@users.noreply.github.com>
2024-11-26 00:47:30 +0300

    Add files via upload

---
+++ ArrayList/ArrayList/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+
+ArrayList arrayList = new ArrayList()
+{
+    10,"BTK Akademi",true,'e'
+};
+
+
+foreach (var item in arrayList)
+{
+    Console.Write($"{item} ");
+}
+
+Console.WriteLine();
+
+int[] numbers = new int[]
+{
+    23,44,55
+};
+arrayList.AddRange(numbers);
+
+foreach (var item in arrayList)
+{
+    Console.Write($"{item} ");
+}
+Console.WriteLine();
+
+Console.WriteLine(arrayList[4]);
+
+int x = (int)arrayList[0];
+Console.WriteLine(x+10);
+
+//Delete
+//arrayList.Remove(10);
+//arrayList.RemoveAt(1);
+arrayList.RemoveRange(1,1);
+
+foreach (var item in arrayList)
+{
+    Console.Write($"{item} ");
+}
+Console.WriteLine();
