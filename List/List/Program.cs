8217a0d8ef639a2809566c58e0d086a174642463
Cemil Ozcan <71188464+cemil3535@users.noreply.github.com>
2024-11-27 14:32:13 +0300

    Add files via upload

---
+++ List/List/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+List<int> numbers = new List<int>();
+numbers.Add(10);
+numbers.Add(15);
+numbers.Add(20);
+
+int x = 55;
+int[] serial = new int[]
+    {
+        70,
+        80,
+        90
+    };
+
+numbers.Add(x);
+numbers.AddRange(serial);
+
+numbers.Insert(3, 0);
+numbers.RemoveAt(2);
+numbers.Remove(55);
+numbers.RemoveAt(numbers.IndexOf(0));
+foreach (var number in numbers)
+{
+    Console.Write($"{number,-5}");
+}
+
+Console.WriteLine("-----------------------");
