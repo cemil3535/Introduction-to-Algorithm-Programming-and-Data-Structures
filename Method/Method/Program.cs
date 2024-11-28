6e76b64d73b9af7e2629fef6b2b046699a0c277b
Cemil Ozcan <71188464+cemil3535@users.noreply.github.com>
2024-11-28 22:02:27 +0300

    Add files via upload

---
+++ Method/Method/Program.cs
@@ -0,0 +1,14 @@
+﻿double sum = SerialToplam(5.5, 6.7, 8.9, 11.498);
+
+Console.WriteLine("{0,5:0.##}",sum);
+
+static double SerialToplam(params double[] serials)
+{
+    double sum = 0;
+    foreach (var serial in serials)
+    {
+        sum += serial;
+
+    }
+    return sum;
+}
