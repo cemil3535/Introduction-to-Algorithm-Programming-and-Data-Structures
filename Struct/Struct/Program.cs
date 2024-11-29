d305a1f9ca9c93b001b9ed1cef4483dc314516af
Cemil Ozcan <71188464+cemil3535@users.noreply.github.com>
2024-11-29 23:13:26 +0300

    Add files via upload

---
+++ Struct/Struct/Program.cs
@@ -0,0 +1,62 @@
+﻿/*
+- We use the struct structure to combine one or more types of data.
+- Value type.
+- Used on small scale data.
+- No inheritance.
+- Interface supports inheritance.
+- No boxing/unboxing.
+- Parameterized constructor method, constants, fields, properties can be used.
+*/
+
+
+// Struct use first
+
+using System.Threading.Channels;
+
+Student student1 = new Student();
+student1.Number = 10;
+student1.Name = "Ali";
+student1.Surname = "Oz";
+student1.Gender = true;
+
+
+
+//  Struct use second
+var student2 = new Student()
+{
+    Number = 20,
+    Name = "Fatma",
+    Surname = "Dag",
+    Gender = false
+};
+
+
+
+// Struct use three
+
+// var student3 = new Student(30, "Mehmet", "Avsar", true);
+
+// var student4 = new Student(40, "Asaf", "Karlidag");
+
+
+Console.WriteLine(student1);
+Console.WriteLine(student2);
+//Console.WriteLine(student3);
+//Console.WriteLine(student4);
+
+List<Student> students = new List<Student>()
+{
+    new Student(30,"Mehmet","Avsar",true),
+    new Student(40,"Asaf","Karlidag")
+
+};
+
+students.ForEach(s => Console.WriteLine(s));
+
+
+/*
+foreach (var student in students)
+{
+    Console.WriteLine(student);
+}
+*/
---
+++ Struct/Struct/Student.cs
@@ -0,0 +1,41 @@
+﻿/*
+- We use the struct structure to combine one or more types of data.
+- Value type.
+- Used on small scale data.
+- No inheritance.
+- Interface supports inheritance.
+- No boxing/unboxing.
+- Parameterized constructor method, constants, fields, properties can be used.
+*/
+
+public struct Student
+{
+    // Constructor
+    public Student(int number, string name, string surname, bool gender = true)
+    {
+        // The constructor method will be run automatically when the object is created.
+        Number = number;
+        Name = name;
+        Surname = surname;
+        Gender = gender;
+    }
+
+    // Property
+    public int Number { get; set; } 
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public bool Gender { get; set; }
+
+
+    // override
+    public override string ToString()
+    {
+        return $"{Number,-5} " +
+            $"{Name,-10} " +
+            $"{Surname,-10} " +
+            string.Format("{0,-8}", Gender == true ? "Male" : "Female");         
+    }
+}
+
+
+
