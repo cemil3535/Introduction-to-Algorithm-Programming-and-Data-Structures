668d7b4bc98f8966d6ffe503177103b542021da7
Cemil Ozcan <71188464+cemil3535@users.noreply.github.com>
2024-11-30 23:22:44 +0300

    Add files via upload

---
+++ StructTwo/StructTwo/Program.cs
@@ -0,0 +1,32 @@
+﻿
+var p1 = new Point(3,4);
+Console.WriteLine(p1);
+
+p1.SetOrigin();
+Console.WriteLine(p1);
+
+
+
+public struct Point
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public override string ToString()
+    {
+        return $"{X},{Y}";
+    }
+
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public void SetOrigin()
+    {
+        X = 0;
+        Y = 0;
+    }
+
+}
