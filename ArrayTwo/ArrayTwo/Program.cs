63612d10dce09c710f5048d06a8b62d54ca7df91
Cemil Ozcan <71188464+cemil3535@users.noreply.github.com>
2024-12-01 22:50:04 +0300

    Add files via upload

---
+++ ArrayTwo/ArrayTwo/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int[] numbers = new int[]
+{
+    5,3,8,10,2
+};
+
+
+var numbers1 = Array.CreateInstance(typeof(int), 5);
+numbers1.SetValue(-5, 0);
+numbers1.SetValue(3, 1);
+numbers1.SetValue(8, 2);
+numbers1.SetValue(10, 3);
+numbers1.SetValue(-2, 4);
+
+for (int i = 0; i < numbers1.Length; i++)
+{
+    Console.WriteLine("number[{0}] = {1} - numbers1[{0}] = {2}" , i, numbers[i], numbers1.GetValue(i));
+}
