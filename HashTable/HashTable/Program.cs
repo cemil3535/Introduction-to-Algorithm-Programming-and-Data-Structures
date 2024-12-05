122fced5c7312d5e3c691900a930de14e0f0d3ab
Cemil Ozcan <71188464+cemil3535@users.noreply.github.com>
2024-12-05 23:48:49 +0300

    Add files via upload

---
+++ HashTable/HashTable/Program.cs
@@ -0,0 +1,9 @@
+﻿/*
+- System Collections sinifi altinda yer alir.
+- Non-generic object, boxing/unboxing
+- Indisleme yok
+- Key value pairs Anahtar - Deger cifti
+- Key - Value  ICollection (Interface)
+- Anahtar Contein Key
+Deger Contein Value
+ */
