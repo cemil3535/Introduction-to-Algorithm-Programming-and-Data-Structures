cd14b13f96ce08e5435833ed70ebc551b91b7f23
Cemil Ozcan <71188464+cemil3535@users.noreply.github.com>
2025-02-10 00:05:00 +0300

    Add files via upload

---
+++ HashTable_Temelleri/HashTable_Temelleri/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+
+// Tanimlama
+var sehirler = new Hashtable();
+
+
+// Ekleme
+sehirler.Add(6, "Ankara");
+sehirler.Add(34, "Istanbul");
+sehirler.Add(55, "Samsun");
+sehirler.Add(23, "Elazig");
+
+// Dolasma
+foreach (DictionaryEntry item in sehirler)
+{
+    Console.WriteLine($"{item.Key,-5} " + $"- {item.Value,-20}");
+}
+
+// Anahtarlari alma
+Console.WriteLine("\nAnahtarlar (Keys)");
+var anahtarlar = sehirler.Keys;
+foreach(var item in anahtarlar)
+{
+    Console.WriteLine(item);
+}
+
+// Degerler
+Console.WriteLine("\nDegerler (values)");
+ICollection degerler = sehirler.Values;
+foreach(var item in degerler)
+{
+    Console.WriteLine(item);
+}
+
+// Elemana Erisme
+Console.WriteLine("\nElemana erisim");
+Console.WriteLine(sehirler[55]);
+
+
+//Eleman silme
+Console.WriteLine("\n Eleman silme");
+sehirler.Remove(23);
+
+// Dolasma
+foreach(DictionaryEntry item in sehirler)
+{
+    Console.WriteLine($"{item.Key,-5}" + $"- {item.Value,-20}");
+}
